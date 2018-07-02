1c9fd9679dcd19c099e927d160a23728e1763324
Dagiel, Patryk (Nokia - PL/Wroclaw) <patryk.dagiel@nokia.com>
2018-07-02 16:33:47 +0200

    Initial commit - first test

---
+++ src/main/java/com/dagiel/blog/service/PostService.java
@@ -0,0 +1,24 @@
+package com.dagiel.blog.service;
+
+import com.dagiel.blog.entities.Post;
+import com.dagiel.blog.repositories.PostRepository;
+import javafx.geometry.Pos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    public PostRepository postRepository;
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public void insert(Post post) {
+        postRepository.save(post);
+    }
+}
